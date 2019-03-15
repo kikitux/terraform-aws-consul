d916e380bd2928c9a465cc54904360eada774c67
Denov <denov@Denovs-MBP.zyxel.com>
2019-03-15 17:48:19 +0200

    adding

--- scripts/conf-dnsmasq.sh
+++ scripts/conf-dnsmasq.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 apt-get install dnsmasq -y
 cat << EOF > /etc/dnsmasq.d/10-consul
-server=/denislav/127.0.0.1#8600
+server=/consul/127.0.0.1#8600
 EOF
 
 sed -i -e 's/#resolv-file=/resolv-file=\/etc\/dnsmasq.d\/outside.conf/g' /usr/share/doc/dnsmasq-base/examples/dnsmasq.conf.example
